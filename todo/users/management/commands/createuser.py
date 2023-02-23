4b3ceb3aab859b8e9b7b0ab2a3686e4d3c1b3614
dilbazi <you@example.com>
2023-02-23 13:06:06 +1100

    lesson1finish1

--- todo/users/management/commands/createuser.py
+++ todo/users/management/commands/createuser.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand
 from users.models import User
+
+
 class Command(BaseCommand):
+    help = "create user and suoeruser"
     def handle(self, *args, **options):
         User.objects.all().delete()
-        User.objects.create_superuser('dilbazi','dilbazi1@bk.ru',"mardaliyeva")
-        User.objects.create_user('dilbazi1','dilbazi11@bk.ru',"mardaliyeva1")
-        User.objects.create_user('dilbazi101','dilbazi101@bk.ru',"mardaliyeva101")
+        User.objects.create_superuser('dilbazi', 'dilbazi1@bk.ru', "mardaliyeva")
+        User.objects.create_user('dilbazi1', 'dilbazi11@bk.ru', "mardaliyeva1")
+        User.objects.create_user('dilbazi101', 'dilbazi101@bk.ru', "mardaliyeva101")
         print('done')
